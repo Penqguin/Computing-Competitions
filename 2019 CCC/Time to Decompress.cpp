1441c430461c24bd24c5f644d7a55e76d1a3b5ca
Michael M <mpmlearn2@gmail.com>
2024-11-05 08:55:09 -0500

    cpp solution

--- 2019 CCC/Time to Decompress.cpp
+++ 2019 CCC/Time to Decompress.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
     int l;
+    int n;
+    char x;
     cin >> l;
     for(int i = 0; i < l; i++) {
-        int n;
-        char x;
         cin >> n >> x;
         cout << string(n, x) << endl;
     }
-    
+
     return 0;
 }
