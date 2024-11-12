22d533a43f7901e67c2f867859cd4ad3df836112
Michael M <mpmlearn2@gmail.com>
2024-11-12 15:01:07 -0500

    started working on this

---
+++ 2015 CCC/pi day.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, k;
+
+  scanf("%d", &n);
+  scanf("%d", &k);
+
+  if (k == n){
+    printf("%d", n);
+  }
+  else if (k == 1){
+    printf("%d", k);
+  }
+  else{
+    
+  }
+}
