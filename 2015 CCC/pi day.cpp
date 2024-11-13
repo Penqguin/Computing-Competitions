0e3deff98be29251f639ecf64a427350e8d9c3b1
Michael M <mpmlearn2@gmail.com>
2024-11-13 14:04:12 -0500

    done

--- 2015 CCC/pi day.cpp
+++ 2015 CCC/pi day.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int countWays(int n, int k);
+
 int main() {
   int n, k;
-
   scanf("%d", &n);
   scanf("%d", &k);
 
-  if (k == n){
-    printf("%d", n);
+  int result = countWays(n, k);
+  printf("%d\n", result);
+  return 0;
+}
+
+int countWays(int n, int k) {
+  vector<vector<int>> slices(n + 1, vector<int>(k + 1, 0));
+
+  // Base cases:
+  for (int i = 1; i <= n; i++) {
+    slices[i][1] = 1; // Only one way to give i pieces to 1 person
   }
-  else if (k == 1){
-    printf("%d", k);
+  for (int i = 1; i <= k; i++) {
+    slices[i][i] = 1; // Only one way to distribute i pieces to i people (each gets one piece)
   }
-  else{
-    
+  for (int j = 2; j <= k; j++) { // Number of people
+    for (int i = j; i <= n; i++) { // Number of pieces
+      slices[i][j] = slices[i - 1][j - 1] + slices[i - j][j];
+    }
   }
+
+  return slices[n][k];
 }
